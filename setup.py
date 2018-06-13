ada89a6f9d1d72a389e9c6cf7b44dae065107c21
Logan Grado <grado@umn.edu>
2018-06-13 17:01:09 -0500

    Updated long_description for pypi

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ with open('loadTDT/__init__.py', 'r') as file:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         file.read(), re.MULTILINE).group(1)
 
+long_description = open('README.md').read()
+
 setup(name         = 'loadTDT',
       version      = version,
       description  = 'Read a TDT tank into a python dictionary',
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
       url          = 'https://github.com/logangrado/loadTDT',
       author       = 'Logan Grado',
       author_email = 'grado@umn.edu',
